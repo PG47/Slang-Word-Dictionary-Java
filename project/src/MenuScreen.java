db05fe5c8358a6d7e60fbfec5803815ff7251c76
PG47 <86549123+PG47@users.noreply.github.com>
2023-11-03 23:18:16 +0700

    Begin to create project

---
+++ project/src/MenuScreen.java
@@ -0,0 +1,22 @@
+package slang;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class MenuScreen {
+    
+}
