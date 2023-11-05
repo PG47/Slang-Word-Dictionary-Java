e19d11615052f08941f7d2efc8b2273eb84dbe65
PG47 <86549123+PG47@users.noreply.github.com>
2023-11-05 17:04:19 +0700

    making in slag reading process 1

--- project/src/Slang.java
+++ project/src/Slang.java
@@ -16,4 +16,46 @@ public class Slang {
     private String FILE="slangword.txt";
     private String ORIGINAL_FILE = "original-slangword.txt";
 
+    private Slang() {
+        try {
+            ReadFile(FILE);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void ReadFile(String file_name) throws Exception {
+        map.clear();
+        String slag=null;
+        Scanner scan = new Scanner(new File(file_name));
+        scan.useDelimiter("`");
+        scan.next();
+        String temp = scan.next();
+        String[] part = temp.split("\n");
+        size_map=0;
+        while(scan.hasNext()) {
+            List<String> meaning = new ArrayList<String>();
+            slag = part[1].trim();
+            temp = scan.next();
+            part = temp.split("\n");
+            if (map.containsKey(slag)) {
+                meaning = map.get(slag);
+            }
+
+            //If slag have more than 1 mean
+            if (part[0].contains("|")) {
+                System.out.println(part[0]);
+                String[] d = (part[0]).split("\\|");
+                for (int ii = 0; ii < d.length; ii++)
+                    System.out.println(d[ii]);
+                Collections.addAll(meaning, d);
+                size_map += d.length - 1;
+            } else {
+                meaning.add(part[0]);
+            }
+            map.put(slag, meaning);
+            size_map++;
+        }
+        scan.close();
+    }
 }
