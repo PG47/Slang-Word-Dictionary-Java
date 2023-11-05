acd7a955f5595946b8b2542b964d62099a8f7ab3
PG47 <86549123+PG47@users.noreply.github.com>
2023-11-05 11:19:06 +0700

    Create original slangword for reset function later

---
+++ project/src/Slang.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class Slang {
+    private TreeMap<String, List<String>> map = new TreeMap<>();
+    private static Slang obj = new Slang();
+    private int size_map;
+    private String FILE="slangword.txt";
+    private String ORIGINAL_FILE = "original-slangword.txt";
+
+}
